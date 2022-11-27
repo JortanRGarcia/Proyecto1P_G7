ba0caf7f896bdb3dbb2a585332e630da73bb32f1
JortanRGarcia <jorgsant@espol.edu.ec>
2022-11-27 13:10:13 -0500

    Corrección Cliente
    Se añadió propiedad numeroTarjeta faltante

--- src/usuarios/Cliente.java
+++ src/usuarios/Cliente.java
@@ -2,19 +2,19 @@ package usuarios;
 
 public class Cliente extends Usuario {
 
+    private final String numeroTarjeta;
     private final TipoVip tipoVip;
     private final int millas;
 
-    public Cliente(String cedula, String nombres, String apellidos, int edad, String correo, String usuario, String contraseña) {
+    public Cliente(String cedula, String nombres, String apellidos, int edad, String correo, String usuario, String contraseña, String numeroTarjeta, TipoVip tipoVip, int millas) {
         super(cedula, nombres, apellidos, edad, correo, usuario, contraseña);
-        this.tipoVip = null;
-        this.millas = 0;
-    }
-
-    public Cliente(String cedula, String nombres, String apellidos, int edad, String correo, String usuario, String contraseña, TipoVip tipoVip, int millas) {
-        super(cedula, nombres, apellidos, edad, correo, usuario, contraseña);
+        this.numeroTarjeta = numeroTarjeta;
         this.tipoVip = tipoVip;
         this.millas = millas;
+    }
+
+    public Cliente(String cedula, String nombres, String apellidos, int edad, String correo, String usuario, String contraseña, String numeroTarjeta) {
+        this(cedula, nombres, apellidos, edad, correo, usuario, contraseña, numeroTarjeta, null, 0);
     }
 
     public TipoVip getTipoVip() {
